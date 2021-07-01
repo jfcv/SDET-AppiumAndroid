95b2b0fee94b580dc1425c96d08188819144d0d3
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-01 09:17:10 -0500

    section 3, lesson 19

---
+++ src/test/java/Base.java
@@ -0,0 +1,28 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Base {
+
+    public static void main(String[] args) throws MalformedURLException {
+
+        final String deviceName = "phone10";
+
+        AndroidDriver<AndroidElement> driver;
+
+        File dir = new File("app");
+        File file = new File(dir, "ApiDemos-debug.apk");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+        caps.setCapability(MobileCapabilityType.APP, file.getAbsolutePath());
+
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    }
+}
