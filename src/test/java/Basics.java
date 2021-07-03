106f520c7abe3699645389208210109acbd91680
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-03 08:55:35 -0500

    automating app UI using xpath

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,10 +1,18 @@
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
 
 public class Basics extends Base {
     public static void main(String[] args) throws MalformedURLException {
         AndroidDriver<AndroidElement> driver = Capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        /* xpath syntax
+         * tagname[@attribute='value'] tagname : same as class on the appium inspector attributes
+         */
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
     }
 }
