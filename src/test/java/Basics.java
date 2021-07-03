e781a79ccc40ed6c1788f4d53f3bdb1eca039287
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-03 09:28:31 -0500

    find elements using the classNames attribute.

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -18,5 +18,7 @@ public class Basics extends Base {
         driver.findElementByXPath("//android.widget.TextView[@text='3. Preference dependencies']").click();
         driver.findElementById("android:id/checkbox").click();
         driver.findElementByXPath("(//android.widget.RelativeLayout)[2]").click();
+        driver.findElementByClassName("android.widget.EditText").sendKeys("hello");
+        driver.findElementsByClassName("android.widget.Button").get(1).click();
     }
 }
