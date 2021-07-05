ed2269ead7f52a5959e6507b5312e974eeeae9a5
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-05 09:11:40 -0500

    longPress gesture.

--- src/test/java/MobileGestures.java
+++ src/test/java/MobileGestures.java
@@ -5,11 +5,13 @@ import io.appium.java_client.android.AndroidElement;
 
 import static io.appium.java_client.touch.TapOptions.tapOptions;
 import static io.appium.java_client.touch.offset.ElementOption.element;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static java.time.Duration.ofSeconds;
 
 import java.net.MalformedURLException;
 
 public class MobileGestures extends Base {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         AndroidDriver<AndroidElement> driver = Capabilities();
 
         driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
@@ -18,5 +20,9 @@ public class MobileGestures extends Base {
         TouchAction action = new TouchAction(driver);
         MobileElement expandableList = driver.findElementByAccessibilityId("Expandable Lists");
         action.tap(tapOptions().withElement(element(expandableList))).perform();
+        driver.findElementByAccessibilityId("1. Custom Adapter").click();
+        MobileElement peopleNamesOption = driver.findElementByXPath("//android.widget.TextView[@text='People Names']");
+        action.longPress(longPressOptions().withElement(element(peopleNamesOption)).withDuration(ofSeconds(2))).release().perform();
+        System.out.println(driver.findElementById("android:id/title").isDisplayed());
     }
 }
