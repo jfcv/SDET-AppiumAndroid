54ccd8939697a676b8496aaaadefdbbb341a633f
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-09 09:27:16 -0500

    GeneralStoreTestCase3 implemented. Another way to scrolling into view but this one makes sure the complete product is displayed.

---
+++ src/test/java/GeneralStoreTestCase3.java
@@ -0,0 +1,31 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+import java.net.MalformedURLException;
+
+public class GeneralStoreTestCase3 extends Base {
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = Capabilities(true);
+
+        // locators
+        MobileElement countryDropdown = driver.findElementById("com.androidsample.generalstore:id/spinnerCountry");
+        MobileElement inputText = driver.findElementById("com.androidsample.generalstore:id/nameField");
+        MobileElement radioFemale = driver.findElementById("com.androidsample.generalstore:id/radioFemale");
+        MobileElement shopButton = driver.findElementById("com.androidsample.generalstore:id/btnLetsShop");
+
+        // positive path flow
+        inputText.click();
+        inputText.sendKeys("hello");
+        driver.hideKeyboard();
+        radioFemale.click();
+        countryDropdown.click();
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"))");
+        driver.findElementByXPath("//android.widget.TextView[@text='Argentina']").click();
+        shopButton.click();
+        String productName = "Jordan 6 Rings";
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()" +
+                ".resourceId(\"com.androidsample.generalstore:id/rvProductList\"))" +
+                ".scrollIntoView(new UiSelector().text(\""+ productName + "\"));");
+    }
+}
