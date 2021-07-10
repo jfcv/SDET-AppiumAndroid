1bc63f6170ae63b0cc8187f18b1959a356964740
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-10 10:19:22 -0500

    A way to finding elements with repeated locator and pick one of them to execute actions on that specific one.

--- src/test/java/GeneralStoreTestCase3.java
+++ src/test/java/GeneralStoreTestCase3.java
@@ -1,6 +1,7 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
 
 import java.net.MalformedURLException;
 
@@ -23,9 +24,20 @@ public class GeneralStoreTestCase3 extends Base {
         driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"))");
         driver.findElementByXPath("//android.widget.TextView[@text='Argentina']").click();
         shopButton.click();
-        String productName = "Jordan 6 Rings";
+        String product = "Jordan 6 Rings";
         driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()" +
                 ".resourceId(\"com.androidsample.generalstore:id/rvProductList\"))" +
-                ".scrollIntoView(new UiSelector().text(\""+ productName + "\"));");
+                ".scrollIntoView(new UiSelector().text(\""+ product + "\"));");
+        // picking a product dynamically from the list
+        int count = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+        for (int i=0;i<count;i++) { //O(n) time complexity
+            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+            if (productName.equalsIgnoreCase("Jordan 6 Rings")) {
+                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+                break;
+            }
+        }
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
     }
 }
