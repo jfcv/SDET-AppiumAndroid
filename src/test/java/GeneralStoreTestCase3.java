da086a951207f1efd02edc8e4de3be28ff07e81a
CUADJ011 <juan.x.cuadros.-nd@disney.com>
2021-07-12 09:15:30 -0500

    [validating the orders in the checkout page] test case. found and learned about the Stale Element Reference Exception implemented that case.

--- src/test/java/GeneralStoreTestCase3.java
+++ src/test/java/GeneralStoreTestCase3.java
@@ -2,11 +2,12 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 
 import java.net.MalformedURLException;
 
 public class GeneralStoreTestCase3 extends Base {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         AndroidDriver<AndroidElement> driver = Capabilities(true);
 
         // locators
@@ -32,12 +33,26 @@ public class GeneralStoreTestCase3 extends Base {
         int count = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
         for (int i=0;i<count;i++) { //O(n) time complexity
             String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
-            if (productName.equalsIgnoreCase("Jordan 6 Rings")) {
+            if (productName.equalsIgnoreCase(product)) {
                 driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
                 break;
             }
         }
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-
+        /*
+         * Stale Element Reference Exception
+         * it's happening because it tries to find the object immediately but needs some
+         * time to find the element in the new view
+         * And this is because there was another element with the same locator attributes
+         * in the previous view so it thinks it's trying to use the element from
+         * the previous view
+         * for more info: https://www.selenium.dev/exceptions/#stale_element_reference.html
+         *
+         * Solution
+         * invoke a thread.sleep for at least a second to give it time to process the new view
+         */
+        Thread.sleep(1000);
+        String productNameOnCart = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(productNameOnCart, product);
     }
 }
